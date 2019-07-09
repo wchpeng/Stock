e0b39a864b2bfe98fa57df5decd6406244652710
smartwu_sir <smartwu_sir@163.com>
2019-07-09 14:45:00 +0800

    修改 data app 的 admin 配置

--- data/admin.py
+++ data/admin.py
@@ -3,6 +3,25 @@ from django.contrib import admin
 from data.models import Stock, StockHistory, StockRecommend
 
 
-admin.site.register(Stock)
-admin.site.register(StockHistory)
+class StockAdmin(admin.ModelAdmin):
+
+    list_display = ['code', 'name', 'enum_stock_type', 'now', 'low', 'high', 'datetime', 'update_time', 'create_time']
+    search_fields = ['code', 'name']
+    ordering = ('-now', 'id')
+
+
+class StockHistoryAdmin(admin.ModelAdmin):
+
+    list_display = ['code', 'now', 'low', 'high', 'datetime', 'update_time', 'create_time']
+    search_fields = ['code']
+
+
+class StockRecommendAdmin(admin.ModelAdmin):
+
+    list_display = ['stock_code', 'stock_name', 'strategy', 'latest', 'create_time']
+    search_fields = ['stock_code', 'stock_name']
+
+
+admin.site.register(Stock, StockAdmin)
+admin.site.register(StockHistory, StockHistoryAdmin)
 admin.site.register(StockRecommend)
